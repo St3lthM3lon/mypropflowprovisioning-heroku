33a1a1dd3ab8bfad9fffee64f9fd452d6547ad9a
St3lthM3lon <lapinskyjaxon@gmail.com>
2025-06-10 21:09:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,10 +34,16 @@ def provision():
             logging.error("Missing or invalid JSON payload.")
             abort(400, "Missing JSON")
 
-
+    
         logging.info(f"Received provision payload: {data}")
-        client_name = data["clientName"]
+        client_name = data.get("clientName")
+            if not client_name:
+                logging.error("Missing field 'clientName' in payload: %s", data)
+                abort(400, "Missing field: clientName")
         client_email = data.get("email", "")
+            if not client_email: 
+                logging.error("Missing field 'clientEmail' in payload: %s", datat)
+                abort(400, "Missing field: clientName")
         client_phone = data.get("phone", "")
         workspace_id = data.get("workspaceId", "")
 
